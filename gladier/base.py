419186136e5c601ca6e9e313c44e8606214406a9
Rafael Vescovi <ravescovi@gmail.com>
2021-03-18 12:37:37 -0500

    base update

--- gladier/base.py
+++ gladier/base.py
@@ -1,8 +1,7 @@
 from .client import GladierClient as GladierBaseClient
 
-
 ##not sure how to update this.
-# class GladierBaseClient(object):
+# class GladierBaseClient(GladierClient):
 
 #     container_url = None
 #     container_type = None
@@ -14,8 +13,13 @@ class GladierBaseTool(object):
     flows, funcx-functions, and default inputs for starting a flow."""
 
     flow_definition = None
-    flow_input = dict()
+
+    required_input = []
+    
+    funcx_endpoints = dict()
     funcx_functions = []
+
+
 
 class GladierBaseContainer(object):
 
