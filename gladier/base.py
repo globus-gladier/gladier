5a6307bdd5ff4cbc63086052f64c456d5f188b09
Rafael Vescovi <ravescovi@gmail.com>
2021-04-12 16:23:31 -0500

    Clean Container

--- gladier/base.py
+++ gladier/base.py
@@ -10,19 +10,3 @@ class GladierBaseTool(object):
     funcx_endpoints = dict()
     funcx_functions = []
 
-    def check_endpoints(self):
-        pass
-
-    def check_globus(self):
-        pass
-
-
-class GladierBaseContainer(object):
-
-    container_url = None
-    container_type = None
-    container_location = None
-    container_flags = None
-
-    def check_existance(self):
-        pass
