3ef07bcade83952762b1759697bb6ce231c61372
Nickolaus D. Saint <nick@globus.org>
2021-05-14 16:48:25 -0700

    @generate_flow_definition now a top level import

--- gladier/__init__.py
+++ gladier/__init__.py
@@ -1,9 +1,9 @@
 import logging
+from gladier.client import GladierBaseClient
+from gladier.base import GladierBaseTool
+from gladier.decorators import generate_flow_definition
+
+__all__ = [GladierBaseClient, GladierBaseTool, generate_flow_definition]
 
 # https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library  # noqa
 logging.getLogger("gladier").addHandler(logging.NullHandler())
-
-from .client import GladierBaseClient
-from .base import GladierBaseTool
-
-__all__ = [GladierBaseClient, GladierBaseTool]
