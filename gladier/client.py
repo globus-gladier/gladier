61a45d37b3d96e1224bc4168d3586c50ea95ca6a
Rafael Vescovi <ravescovi@gmail.com>
2021-07-20 15:21:13 -0500

    bug fix

--- gladier/client.py
+++ gladier/client.py
@@ -17,7 +17,6 @@ import gladier.utils.dynamic_imports
 import gladier.utils.automate
 import gladier.utils.name_generation
 import gladier.utils.config_migrations
-import gladier.utils.flow_generation
 import gladier.exc
 import gladier.version
 log = logging.getLogger(__name__)
@@ -310,6 +309,8 @@ class GladierBaseClient(object):
         raise gladier.exc.ConfigException('"flow_definition" must be a dict or an import string '
                                           'to a sub-class of type '
                                           '"gladier.GladierBaseTool"')
+
+
 
     def get_flow_checksum(self):
         """
