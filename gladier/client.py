bf9eb801446b8dc85f6dddce4101264229068861
Nickolaus D. Saint <nick@globus.org>
2021-07-22 14:05:10 -0700

    fix: Pass an 'empty' schema by default to fulfill automate requirement

--- gladier/client.py
+++ gladier/client.py
@@ -479,6 +479,9 @@ class GladierBaseClient(object):
         }
         log.debug(f'Flow permissions set to: {flow_permissions or "Flows defaults"}')
         flow_kwargs = flow_permissions
+        # Input schema will be (probably is now) a required field. Returning {} is a temporary
+        # fix to avoid an automate error, until we can properly generate an input schema.
+        flow_kwargs['input_schema'] = {}
         if self.subscription_id:
             flow_kwargs['subscription_id'] = self.subscription_id
         if flow_id:
