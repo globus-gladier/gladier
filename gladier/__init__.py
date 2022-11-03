b905e7c3f1eca559d4a2c8cb62f545c01bdf7c19
Nickolaus D. Saint <nick@globus.org>
2022-11-03 15:13:22 -0700

    feat: Make the flows manager available for public usage
    The FlowsManager class allows for customizing some behavior of how
    flows are deployed and run.

--- gladier/__init__.py
+++ gladier/__init__.py
@@ -2,8 +2,9 @@ import logging
 from gladier.client import GladierBaseClient
 from gladier.base import GladierBaseTool
 from gladier.decorators import generate_flow_definition
+from gladier.managers import FlowsManager
 
-__all__ = [GladierBaseClient, GladierBaseTool, generate_flow_definition]
+__all__ = [GladierBaseClient, GladierBaseTool, generate_flow_definition, FlowsManager]
 
 # https://docs.python.org/3/howto/logging.html#configuring-logging-for-a-library  # noqa
 logging.getLogger("gladier").addHandler(logging.NullHandler())
