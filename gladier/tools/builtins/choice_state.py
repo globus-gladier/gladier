fd41a2bc3311349bc109f8f262e81bac59b65c78
Jim Pruyne <pruyne@uchicago.edu>
2023-06-05 12:27:54 -0500

    WIP: Starting on Choice State

--- gladier/tools/builtins/choice_state.py
+++ gladier/tools/builtins/choice_state.py
@@ -2,7 +2,13 @@ from __future__ import annotations
 
 import typing as t
 
+from pydantic import BaseModel
+
 from gladier import GladierBaseState, JSONObject
+
+
+class ChoiceOption(BaseModel):
+    next: GladierBaseState
 
 
 # TODO: add support here
@@ -12,16 +18,21 @@ class ChoiceState(GladierBaseState):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._choices = []
+        self._choices: t.List[ChoiceOption] = []
 
-    def add_choice(
-        self,
-    ) -> GladierBaseState:
-        # TODO Implement this by adding to self._choices
+    def choice(self, choice_option: ChoiceOption) -> GladierBaseState:
+        self._choices.append(choice_option)
         return self
+
+    def set_default(self, default_choice: GladierBaseState) -> GladierBaseState:
+        self.default = default_choice
+        return self.default
 
     def get_flow_definition(self) -> JSONObject:
         flow_def = super().get_flow_definition()
         state_def = self.get_flow_state_dict()
         # TODO: Add choices and default values to state_def
         return flow_def
+
+    def get_child_states(self) -> t.List[GladierBaseState]:
+        return [self.default] + []
