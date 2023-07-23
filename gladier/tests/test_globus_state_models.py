d39c8c7ffa2c3468bdb418281e56527276597bec
Jim Pruyne <pruyne@uchicago.edu>
2023-07-22 23:01:18 -0500

    Added GlobusTransferState

--- gladier/tests/test_globus_state_models.py
+++ gladier/tests/test_globus_state_models.py
@@ -1,5 +1,5 @@
 from gladier import GladierBaseClient
-from gladier.tools.globus import GlobusComputeStep
+from gladier.tools.globus import GlobusComputeStep, GlobusTransferItem, GlobusTransfer
 
 
 def mock_func(**kwargs):
@@ -17,3 +17,32 @@ def test_globus_compute_state():
     assert flow_def["StartAt"] == compute_step.valid_state_name
     for action_prop_name in {"ActionUrl", "ResultPath", "Type", "End", "Parameters"}:
         assert action_prop_name in flow_def["States"][compute_step.valid_state_name]
+
+
+def test_globus_transfer_state():
+    transfer_step = GlobusTransfer(
+        source_endpoint_id="src",
+        destination_endpoint_id="dest",
+        transfer_items=[
+            GlobusTransferItem(source_path="src_path", destination_path="dest_path")
+        ],
+    )
+
+    flow_def = transfer_step.get_flow_definition()
+
+    assert (
+        flow_def["States"][transfer_step.valid_state_name]["Parameters"][
+            "source_endpoint_id"
+        ]
+        == "src"
+    )
+    assert (
+        flow_def["States"][transfer_step.valid_state_name]["Parameters"][
+            "destination_endpoint_id"
+        ]
+        == "dest"
+    )
+    assert (
+        "transfer_items"
+        in flow_def["States"][transfer_step.valid_state_name]["Parameters"]
+    )
--- gladier/tools/globus/__init__.py
+++ gladier/tools/globus/__init__.py
@@ -3,8 +3,13 @@ from __future__ import annotations
 import typing as t
 
 from .compute import GlobusComputeStep, ComputeFunctionType
+from .transfer import GlobusTransfer, GlobusTransferItem
 
-_nameables = (x.__name__ for x in (GlobusComputeStep,) if hasattr(x, "__name__"))
+_nameables = (
+    x.__name__
+    for x in (GlobusComputeStep, GlobusTransfer, GlobusTransferItem)
+    if hasattr(x, "__name__")
+)
 _unnameables: t.List[str] = ["ComputeFunctionType"]
 
 __all__ = tuple(_nameables) + tuple(_unnameables)
